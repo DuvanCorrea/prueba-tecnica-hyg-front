f7682fa54a284c75b739e22d61bf76f3e767957d
DuvanCorrea <duvancorrea96@gmail.com>
2021-06-25 03:06:30 -0500

    ya casi

--- src/pages/Proyect.jsx
+++ src/pages/Proyect.jsx
@@ -8,10 +8,23 @@ const Proyect = () => {
     const [cargandoInformacion, setCargandoInformacion] = useState(true)
     const [proyecto, setProyecto] = useState({})
     const [seguimientos, setSeguimientos] = useState([])
+    const [seguimiento, setSeguimiento] = useState({
+        fecha: new Date().getFullYear(),
+        descripccion: "",
+        avance: "",
+        PROYECTO_codigo_proyecto: null,
+        estadoProyecto: null,
+    })
 
+    // se pocede a sacar el codigo del proyecto de los parametros de la URL
+    // --------------------------------------------------------------------
     const params = window.location.search
     const urlParams = new URLSearchParams(params)
     const codigoProyecto = urlParams.get("codigo_proyecto")
+    setSeguimiento({
+        ...seguimiento,
+        PROYECTO_codigo_proyecto: codigoProyecto
+    })
 
     useEffect((e) => {
         async function aux() {
@@ -30,9 +43,13 @@ const Proyect = () => {
         aux()
     }, [])
 
-    // se pocede a sacar el codigo del proyecto de los parametros de la URL
-    // --------------------------------------------------------------------
-
+    //Función que se ejecuta cada que el usuario escribe en un input
+    const handleChange = e => {
+        setSeguimiento({
+            ...seguimiento,
+            [e.target.name]: e.target.value
+        })
+    }
 
 
     if (cargandoInformacion) {
@@ -54,7 +71,9 @@ const Proyect = () => {
                             <p className="card-text"><strong>Lider </strong>{proyecto.nombre_lider}</p>
                             <p className="card-text"><strong>Objetivo: </strong>{proyecto.objetivo}</p>
                             <p className="card-text"><strong>Descripción: </strong>{proyecto.descripccion}</p>
-                            <a href="#" className="btn btn-primary">Agregar seguimiento</a>
+                            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Agregar seguimiento
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -91,6 +110,54 @@ const Proyect = () => {
                     }
                 </div>
             </div>
+
+            {/* Modal agregar seguimiento */}
+
+            {/* Modal */}
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Nuevo seguimiento</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            {/* Formulario para agregar seguimiento */}
+
+                            <form onSubmit={(e) => {
+                                e.preventDefault()
+                            }}>
+                                <div className="mb-3">
+                                    <label className="form-label">Descipcción</label>
+                                    <input type="text" className="form-control" id="descripccion" name="descripccion" />
+                                    <div className="form-text">El porque sel seguimiento</div>
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Avance</label>
+                                    <input type="number" className="form-control" id="avance" name="avance" min={1} max={100} />
+                                    <div className="form-text">Actualmente en 10%, ingresar valor de 10 a 100</div>
+                                </div>
+                                <div className="input-group mb-3">
+                                    <select className="form-select" id="estadoProyecto" name="estadoProyecto">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                    <label className="input-group-text" for="inputGroupSelect02">Nuevo estado</label>
+                                </div>
+                            </form>
+
+
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                            <button type="button" className="btn btn-primary">Guardar seguimiento</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
